74b53d1e9cbb9ca41ac25bcfc274141e8f13d16e
Stas Tyschenko <stas7t@gmail.com>
2017-08-27 21:30:28 +0300

    Add views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
+  get 'orders', to: 'orders#index'
+  get 'settings', to: 'settings#index'
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
