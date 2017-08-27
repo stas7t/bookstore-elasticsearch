226ee34313534ad55096e1f505830ac9d754d382
Stas Tyschenko <stas7t@gmail.com>
2017-08-27 18:01:21 +0300

    Add facebook auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,7 +252,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'],
-                  scope: 'email,first_name,last_name'
+                  scope: 'email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
