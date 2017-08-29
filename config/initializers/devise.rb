cbd7f9d923ef6a98f80f8be901df0e39dbd197d7
Stas Tyschenko <stas7t@gmail.com>
2017-08-29 15:26:54 +0300

    Add FB scopes user_friends,user_birthday,user_location

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,7 +252,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'],
-                  scope: 'email'
+                  scope: 'email,user_friends,user_birthday,user_location'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
