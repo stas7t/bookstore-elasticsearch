78d2aae617d45fb6b5f542cbbd6c245a7da3452a
Stas Tyschenko <stas7t@gmail.com>
2017-08-31 16:59:27 +0300

    Update author model and view

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,10 @@
 class Author < ApplicationRecord
   has_and_belongs_to_many :books
+
+  validates :first_name, presence: true
+  validates :first_name, :last_name, length: {
+    maximum: 50,
+    wrong_length: 'Invalid length',
+    too_long: "%{count} characters is the maximum allowed"
+  }
 end
