f8d6ffc1476efa243c1c1bf19f84c1efeddcc32b
Stas Tyschenko <stas7t@gmail.com>
2017-08-31 16:59:57 +0300

    Update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,14 @@ class Category < ApplicationRecord
   has_many :books
 
   validates :name, presence: true
+  validates :text, length: {
+    minimum: 2,
+    maximum: 80,
+    wrong_length: 'Invalid length',
+    too_long: "%{count} characters is the maximum allowed",
+    too_short: "must have at least %{count} characters"
+  }
+  validates :title, :text,
+            format: { with: %r/\A[a-zA-Z0-9 &-\/]*\z/,
+                      message: "must consist of a-z, A-Z, 0-9, or one of &-/" }
 end
