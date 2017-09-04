d6b4f5bca7782ada90afb6b2e6731b6be381d0a1
Stas Tyschenko <stas7t@gmail.com>
2017-09-05 02:10:41 +0300

    Update home ctrl and view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @latest_books = Book.last(3)
+    @categories = Category.all
+    # Temporary decision
+    # TODO: rework to really bestsellers
+    @bestsellers = @categories.map { |category| category.books.first }
   end
 end
