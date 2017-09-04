b538e22ef4351fa020f9a7f964e5f4a858cde49b
Stas Tyschenko <stas7t@gmail.com>
2017-09-05 02:09:59 +0300

    Update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,12 @@
 
 
 
-user = User.create(email: FFaker::Internet.safe_email,
-              password: "P1-#{FFaker::Internet.password}",
-              name: FFaker::Name.name,
-              image: FFaker::Avatar.image)
+User.create!(email: FFaker::Internet.safe_email,
+             password: "test123XX",
+             name: FFaker::Name.name,
+             image: FFaker::Avatar.image)
+
+user = User.last
 
 Category.create(name: 'Mobile development')
 Category.create(name: 'Photo')
@@ -28,26 +30,50 @@ end
 
 authors = Author.all
 
+def book_authors(authors)
+  selected_authors = []
+  rand(1..3).times do
+    author = authors.sample
+    selected_authors << author unless selected_authors.include?(author)
+  end
+  selected_authors
+end
+
+def book_categories(categories)
+  selected_categories = []
+  rand(1..3).times do
+    category = categories.sample
+    selected_categories << category unless selected_categories.include?(category)
+  end
+  selected_categories
+end
+
+
 20.times do
   Book.create(title: FFaker::Book.title,
-              price: [5.99, 10.99, 19.99, 29.99].sample,
+              price: [4.99, 9.99, 14.99, 19.99, 24.99, 29.99].sample,
               quantity: 1,
               description: FFaker::Book.description,
-              height: 7.40,
-              width: 4.50,
-              depth: 0.90,
-              publication_year: FFaker::Vehicle.year,
+              height: rand(7.5...10.0).floor(2),
+              width: rand(4.5...5.5).floor(2),
+              depth: rand(0.3...4.0).floor(2),
+              publication_year: rand(2001..2017),
               materials: FFaker::Lorem.words.join(', '),
-              authors: [authors.sample, authors.sample],
-              categories: [categories.sample])
+              authors: book_authors(authors),
+              categories: book_categories(categories))
 end
 
-book = Book.first
+books = Book.all
 
-3.times do
-  Review.create(title: FFaker::HipsterIpsum.words,
-                text: FFaker::HipsterIpsum.sentences,
-                rating: (1..5).to_a.sample,
-                book_id: book.id,
-                user_id: user.id)
+books.each do |book|
+  rand(1..4).times do
+    user = User.last
+    Review.create!(title: FFaker::Lorem.words.join(', '),
+                  text: FFaker::Lorem.sentences.join('. '),
+                  rating: rand(1..5),
+                  book_id: book.id,
+                  user_id: user.id)
+  end
 end
+
+
