adbc4a3d110d0f2436573cc2a4951516f89decbc
Stas Tyschenko <stas7t@gmail.com>
2017-09-14 21:53:58 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
   resources :authors
   resources :books
+  resources :categories
   resources :reviews
 
-  get 'catalog', to: 'catalog#index'
+  get 'catalog', to: 'books#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
