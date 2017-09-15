b990b5d5d2fff2bfcdac915f06312b610c66a55c
stas7t <stas7t@gmail.com>
2017-09-15 08:18:14 -0400

    Update filtering and sorting

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,15 @@
 class BooksController < ApplicationController
-
   before_action :set_book,       only: [:show, :edit, :update, :destroy]
   before_action :set_categories, only: [:index, :show]
-  before_action :set_current,    only: [:index, :show]
-  after_action  :set_cookies,    only: [:index]
+  before_action :set_current,    only: [:index]
+  include SortOptions
 
   def index
-    @books = Book.all.order(params[:sort_by]).page params[:page] unless params[:category_id]
-    @books = Book.by_category(params[:category_id]).order(params[:sort_by]).page params[:page] if params[:category_id]
+    @books = if @current_category.id 
+      Book.by_category(@current_category.id).order(@sort_option).page params[:page]
+    else
+      Book.order(@sort_option).page params[:page]
+    end
   end
 
   def show; end
@@ -56,13 +58,24 @@ class BooksController < ApplicationController
     @categories = Category.order('name')
   end
 
-  def set_cookies
-    cookies[:current_category] = params[:category_id]
-    cookies[:sort_by]          = params[:sort_by]
-  end
+  #def set_cookies
+  #  cookies[:current_category] = params[:category_id]
+  #  cookies[:sort_by]          = params[:sort_by]
+  #end
 
   def set_current
-    @current_category = params[:category_id] || cookies[:current_category]
-    @sort_by          = params[:sort_by]     || cookies[:sort_by]
+    @current_category = if params[:category_id] && Category.ids.include?(params[:category_id].to_i)
+                           @categories.find(params[:category_id])
+                        else
+                          Category.new(id: nil, name: 'All')
+                        end
+    @sort_by = params[:sort_by]
+    if params[:sort_by] && SortOptions::sort_options.keys.include?(params[:sort_by].to_sym)
+      @sort_name = SortOptions::sort_options[@sort_by.to_sym][:name]
+      @sort_option = SortOptions::sort_options[@sort_by.to_sym][:option] 
+    else 
+      @sort_name = 'Newest first'
+      @sort_option = 'created_at DESC'
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def categories 
+    Category.order('name')
+  end
 end
