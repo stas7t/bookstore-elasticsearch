41565f45389a2524e016d0de97154b8fff83c1a2
Stanislav Tyshchenko <stas7t@gmail.com>
2017-10-09 19:31:46 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
+    :enable_starttls_auto => true,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com'
