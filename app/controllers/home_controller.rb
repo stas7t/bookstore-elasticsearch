e3d2bf642a5b9f069d418e110a14167c6ebd17a5
stas7t <stas7t@gmail.com>
2017-10-11 03:20:51 -0400

    Rework bestsellers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @latest_books = Book.last(3)
-    @bestsellers = Category.ids.map { |c_id| Book.bestsellers(c_id).first }
+    @bestsellers = Book.bestsellers.first(4)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -22,8 +22,8 @@ class Book < ApplicationRecord
   end
 
   def self.bestsellers(category_id = nil)
-    Book.by_category(category_id)
-    # return Book.all.sort_by(&:sales).reverse unless category_id
-    # Book.by_category(category_id).sort_by(&:sales).reverse
+    best = Order.payed.joins(:order_items).group(:book_id)
+                .order('sum("order_items"."quantity") DESC').count.keys
+    Book.find(best)
   end
 end
