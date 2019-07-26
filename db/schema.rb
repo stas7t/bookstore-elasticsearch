18429190369bb699f43219177827f05f756c70da
Stanislav Tyshchenko <stas7t@gmail.com>
2019-07-26 15:58:14 +0300

    add isbn to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013091658) do
+ActiveRecord::Schema.define(version: 20190726105757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20171013091658) do
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.string "cover"
+    t.string "isbn"
     t.index ["category_id"], name: "index_books_on_category_id"
   end
 
