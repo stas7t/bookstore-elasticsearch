155c5ce00f2fae0cb8a28afadf304c76b90ad1fe
Stanislav Tyshchenko <stas7t@gmail.com>
2020-02-09 18:11:22 +0200

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,151 +32,153 @@ end
 
 Admin.create!(email: 'admin@example.com', password: "Qwerty123") if Rails.env.development?
 
-users_count.times do
-  new_user = User.new(
-    email: FFaker::Internet.safe_email,
-    password: "test#{rand(100...999)}XX",
-    name: FFaker::Name.name,
-    remote_avatar_url: FFaker::Avatar.image
-  )
-  new_user.skip_confirmation!
-  new_user.save!
-end
-
-categorise_list.each do |category|
-  Category.create(name: category)
-end
-
-authors_count.times do
-  Author.create(
-    first_name: FFaker::Name.first_name,
-    last_name: FFaker::Name.last_name,
-    description: FFaker::Lorem.paragraph
-  )
-end
-
-categories = Category.all
-
-puts ''
-puts 'create books'
-books_count.times do |index|
-  title = FFaker::Book.title
-  description = Array.new(rand(1..2)).map { |x| FFaker::HipsterIpsum.paragraphs.join(' ') }.join(' ')
-  materials = [
-    'machine-finished coated paper',
-    'woodfree uncoated paper',
-    'coated fine paper',
-    'recycled paper'
-  ]
-
-  new_book = Book.new(
-    title: title,
-    price: "#{rand(1..99)}.99".to_f,
-    description: description,
-    height: rand(7.5...10.0).floor(2),
-    width: rand(4.5...5.5).floor(2),
-    depth: rand(0.3...4.0).floor(2),
-    isbn: FFaker::Book.isbn,
-    publication_year: rand(2001..2017),
-    materials: materials.sample,
-    category_id: categories.sample.id,
-  )
-
-  parts_count = Book.count { |x| x.title.starts_with?(title) }
-  unless parts_count.zero?
-    new_book.title = "#{title} (Part #{parts_count + 1})"
-    if parts_count == 1
-      part_1 = Book.find_by(title: title)
-      part_1.update(title: "#{title} (Part 1)") if part_1.present?
-    end
+unless Rails.env.test?
+  users_count.times do
+    new_user = User.new(
+      email: FFaker::Internet.safe_email,
+      password: "test#{rand(100...999)}XX",
+      name: FFaker::Name.name,
+      remote_avatar_url: FFaker::Avatar.image
+    )
+    new_user.skip_confirmation!
+    new_user.save!
   end
-  new_book.save
-  print '.'
-end
 
-authors = Author.all
-books = Book.all
-users = User.all
-
-puts ''
-puts 'assign authors to books'
-Book.in_batches.each_record do |book|
-  next unless book.authors.empty?
-  book_authors(authors).each do |author|
-    rand(1..max_book_autors).times { Authorship.create!(author_id: author.id, book_id: book.id) }
+  categorise_list.each do |category|
+    Category.create(name: category)
   end
-  print '.'
-end
 
-puts ''
-puts 'create book reviews'
-Book.in_batches.each_record do |book|
-  rand(1..max_book_reviews).times do
-    Review.create!(
-      title: FFaker::Lorem.words.join(', '),
-      text: FFaker::Lorem.sentences.join('. '),
-      rating: rand(1..5),
-      status: rand(0..2),
-      book_id: book.id,
-      user_id: users.sample.id
+  authors_count.times do
+    Author.create(
+      first_name: FFaker::Name.first_name,
+      last_name: FFaker::Name.last_name,
+      description: FFaker::Lorem.paragraph
     )
   end
-  print '.'
-end
 
-puts ''
-puts 'add book covers'
-Book.in_batches.each_record do |book|
-  author_names = book.authors.map { |a| [a.first_name, a.last_name] }
-  author_text = author_names.size > 1 ? author_names.map { |a| "#{a.first[0]}. #{a.last}" }.join(', ') : author_names.join(' ')
+  categories = Category.all
 
-  cover_params = {
-    title: book.title,
-    top_text: book.category.name,
-    author: author_text,
-  }
+  puts ''
+  puts 'create books'
+  books_count.times do |index|
+    title = FFaker::Book.title
+    description = Array.new(rand(1..2)).map { |x| FFaker::HipsterIpsum.paragraphs.join(' ') }.join(' ')
+    materials = [
+      'machine-finished coated paper',
+      'woodfree uncoated paper',
+      'coated fine paper',
+      'recycled paper'
+    ]
 
-  book.update(remote_cover_url: cover_url(cover_params))
-  print '.'
-end
+    new_book = Book.new(
+      title: title,
+      price: "#{rand(1..99)}.99".to_f,
+      description: description,
+      height: rand(7.5...10.0).floor(2),
+      width: rand(4.5...5.5).floor(2),
+      depth: rand(0.3...4.0).floor(2),
+      isbn: FFaker::Book.isbn,
+      publication_year: rand(2001..2017),
+      materials: materials.sample,
+      category_id: categories.sample.id,
+    )
 
-Book.import
-
-Delivery.create(name: 'Delivery Next Day!', time: '3 to  7 days', price:  5.00)
-Delivery.create(name: 'Pick Up In-Store',   time: '5 to 20 days', price: 10.00)
-Delivery.create(name: 'Expressit',          time: '2 to  3 days', price: 15.00)
-
-Coupon.create(code: 'TEST',       discount: 0.01)
-Coupon.create(code: 'WINTERSALE', discount: 5.00)
-Coupon.create(code: 'RUBYGARAGE', discount: 7.50)
-
-puts ''
-puts 'generate orders'
-orders_count.times do
-  order_user = User.limit(1).order("RANDOM()").first
-  ordered_books = Book.limit(rand(1..5)).order("RANDOM()")
-  delivery_type = Delivery.limit(1).order("RANDOM()").first
-  credit_card = CreditCard.new(
-    number: '1111222233334444',
-    name_on_card: FFaker::Name.name,
-    month_year: "#{rand(1..12)}/#{rand(20..25)}",
-    cvv: "#{rand(100..999)}",
-    user_id: order_user.id
-  )
-
-  order_items = ordered_books.map do |book|
-    OrderItem.new(quantity: rand(1..99), book_id: book.id)
+    parts_count = Book.count { |x| x.title.starts_with?(title) }
+    unless parts_count.zero?
+      new_book.title = "#{title} (Part #{parts_count + 1})"
+      if parts_count == 1
+        part_1 = Book.find_by(title: title)
+        part_1.update(title: "#{title} (Part 1)") if part_1.present?
+      end
+    end
+    new_book.save
+    print '.'
   end
 
-  order = Order.new
-  order.user = order_user
-  order.delivery = delivery_type
-  order.credit_card = credit_card
-  order.order_items = order_items
+  authors = Author.all
+  books = Book.all
+  users = User.all
 
-  order.save
-  order.place_in_queue
-  order.update(status: 3)
+  puts ''
+  puts 'assign authors to books'
+  Book.in_batches.each_record do |book|
+    next unless book.authors.empty?
+    book_authors(authors).each do |author|
+      rand(1..max_book_autors).times { Authorship.create!(author_id: author.id, book_id: book.id) }
+    end
+    print '.'
+  end
 
-  print '.'
+  puts ''
+  puts 'create book reviews'
+  Book.in_batches.each_record do |book|
+    rand(1..max_book_reviews).times do
+      Review.create!(
+        title: FFaker::Lorem.words.join(', '),
+        text: FFaker::Lorem.sentences.join('. '),
+        rating: rand(1..5),
+        status: rand(0..2),
+        book_id: book.id,
+        user_id: users.sample.id
+      )
+    end
+    print '.'
+  end
+
+  puts ''
+  puts 'add book covers'
+  Book.in_batches.each_record do |book|
+    author_names = book.authors.map { |a| [a.first_name, a.last_name] }
+    author_text = author_names.size > 1 ? author_names.map { |a| "#{a.first[0]}. #{a.last}" }.join(', ') : author_names.join(' ')
+
+    cover_params = {
+      title: book.title,
+      top_text: book.category.name,
+      author: author_text,
+    }
+
+    book.update(remote_cover_url: cover_url(cover_params))
+    print '.'
+  end
+
+  Book.import
+
+  Delivery.create(name: 'Delivery Next Day!', time: '3 to  7 days', price:  5.00)
+  Delivery.create(name: 'Pick Up In-Store',   time: '5 to 20 days', price: 10.00)
+  Delivery.create(name: 'Expressit',          time: '2 to  3 days', price: 15.00)
+
+  Coupon.create(code: 'TEST',       discount: 0.01)
+  Coupon.create(code: 'WINTERSALE', discount: 5.00)
+  Coupon.create(code: 'RUBYGARAGE', discount: 7.50)
+
+  puts ''
+  puts 'generate orders'
+  orders_count.times do
+    order_user = User.limit(1).order("RANDOM()").first
+    ordered_books = Book.limit(rand(1..5)).order("RANDOM()")
+    delivery_type = Delivery.limit(1).order("RANDOM()").first
+    credit_card = CreditCard.new(
+      number: '1111222233334444',
+      name_on_card: FFaker::Name.name,
+      month_year: "#{rand(1..12)}/#{rand(20..25)}",
+      cvv: "#{rand(100..999)}",
+      user_id: order_user.id
+    )
+
+    order_items = ordered_books.map do |book|
+      OrderItem.new(quantity: rand(1..99), book_id: book.id)
+    end
+
+    order = Order.new
+    order.user = order_user
+    order.delivery = delivery_type
+    order.credit_card = credit_card
+    order.order_items = order_items
+
+    order.save
+    order.place_in_queue
+    order.update(status: 3)
+
+    print '.'
+  end
 end
